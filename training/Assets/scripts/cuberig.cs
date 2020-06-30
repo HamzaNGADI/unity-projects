0b79f18de85b735f1fc77443e9ead1717f0f61b4
HamzaNGADI <ngadihamza@gmail.com>
2020-06-30 21:20:36 +0000

    training and handle framework

---
+++ training/Assets/scripts/cuberig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cuberig : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+}
---
+++ training/Assets/scripts/sc.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sc : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countn;
+    public Text winin;
+    private float rr;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        rr = 0.3f;
+
+        count = 0;
+        countext();
+        winin.text = ""; 
+	}
+
+    private void FixedUpdate()
+    {
+        float moveh = Input.GetAxis("Horizontal");
+        float movev = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveh, 0.0f, movev);
+        rb.AddForce(move*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countext();
+        }
+        if(other.gameObject.CompareTag("cubeappart"))
+        {
+            if (rr < 1f)
+            {
+                other.gameObject.GetComponent<Renderer>().material.color = new Color(rr, 0.3f, 0.24f);
+                rr += 0.08f;
+            }
+            else countn.text = "count : " + count.ToString()+ " -- colors stopped";
+
+        }
+    }
+
+    void countext()
+    {
+        countn.text = "count : " + count.ToString();
+        if(count>=5) winin.text = "Congrats";
+    }
+}
